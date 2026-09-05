f4221059a25a540edaf1860f8f5dfb94c68e156f
Matteo Chauvin <229449054+chauvinmatteo@users.noreply.github.com>
2026-09-05 13:42:34 +0200

    chores(docstrings): added docstrings to models.py

--- src/models.py
+++ src/models.py
@@ -3,14 +3,25 @@ from typing import Dict
 
 
 class Parameters(BaseModel):
+    """ Used to check the validity of the given parameters."""
     type: str
 
 
 class Returns(BaseModel):
+    """ Used to check the validity of the given returns."""
     type: str
 
 
 class FunctionsDef(BaseModel):
+    """
+    Represents the definition of a function available in the AI model.
+
+    Attributes:
+        name (str): exact name of function (ex: 'fn_add_numbers').
+        description (str): function purpose.
+        parameters (Dict[str, Parameters]): dict including needed parameters.
+        returns (Returns): needed object as function return.
+    """
     name: str
     description: str
     parameters: Dict[str, Parameters]
@@ -18,4 +29,5 @@ class FunctionsDef(BaseModel):
 
 
 class FunctionsCalling(BaseModel):
+    """ Used to check the validity of the given prompt."""
     prompt: str
