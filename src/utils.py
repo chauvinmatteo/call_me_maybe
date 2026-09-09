6d6c13e0cc02b518a621fc830101b9d994fa7dd5
Matteo Chauvin <mchauvin@z1r4p2.42lyon.fr>
2026-09-09 15:45:57 +0200

    dev(id_list): added function to avoid repeat of wanted token

--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,13 @@ def get_ids_prefix(prefix: str, vocab: dict[str, int]) -> list[int]:
         if text.startswith(prefix):
             valid_ids.append(token_id)
     return valid_ids
+
+
+def get_ids_free_mode(vocab: dict[str, int]):
+
+    unwanted_ids = ["{", "}", ":", '"']
+    valid_ids = []
+    for text, token_id in vocab.items():
+        if not any(char in text for char in unwanted_ids):
+            valid_ids.append(token_id)
+    return valid_ids
