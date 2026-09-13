4633a963c4f41be8e29c9b30b98570d98828c372
chauvinmatteo <229449054+chauvinmatteo@users.noreply.>
2026-09-13 19:48:09 +0200

    dev(callmemaybe): added algorithm chosen based answer - WIP

--- src/utils.py
+++ src/utils.py
@@ -16,3 +16,19 @@ def get_ids_free_mode(vocab: dict[str, int]):
         if not any(char in text for char in unwanted_ids):
             valid_ids.append(token_id)
     return valid_ids
+
+def get_allowed_name_tokens(valid_names: list[str], vocab: dict[str, int],
+                            current_word: str):
+
+    remaining_suffixes = []
+    valid_ids = []
+    for name in valid_names:
+        if name.startswith(current_word):
+            suffix = name[len(current_word):]
+            remaining_suffixes.append(suffix)
+    for text, token_id in vocab.items():
+        for suffix in remaining_suffixes:
+            if suffix.startswith(text):
+                valid_ids.append(token_id)
+                break
+    return valid_ids
